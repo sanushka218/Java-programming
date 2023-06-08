fec6d85cdb0d43d8664226b9c3e1dd6aa8ea63df
Anushka Saxena <112062928+sanushka218@users.noreply.github.com>
2023-06-08 23:45:39 +0530

    two.java

--- two.java
+++ two.java
@@ -3,11 +3,11 @@ class two {
   public static void main(String arg[]) {
     
     
-   /* char c[]={'A','N','U','S','H','K','A'};
+    char c[]={'A','N','U','S','H','K','A'};
    String str= new String(c);
    System.out.println(str); */
 
-    /*SWAPPING
+    SWAPPING
     int a=9;
     int b=12;
     a=a^b;
@@ -15,9 +15,9 @@ class two {
     a=a^b;
     System.out.println(Integer.toBinaryString(a));
     System.out.println(Integer.toBinaryString(b));
-    */
+    
 
-    /*  STORING 2 4-BIT NO IN A SINGLE BYTE VARIABLE
+    /*STORING 2 4-BIT NO IN A SINGLE BYTE VARIABLE*/
     int a=0;
     int b=5;
     a=a|b;
@@ -27,7 +27,7 @@ class two {
     System.out.println(Integer.toBinaryString(a));
     System.out.println((a&0b00001111));
     System.out.println((a&0b11110000)>>4);
-  */
+  
 
     // sc.close();
 
