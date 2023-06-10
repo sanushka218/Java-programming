e149431be01d1533a348f14bdef9539060b159a0
Anushka Saxena <112062928+sanushka218@users.noreply.github.com>
2023-06-10 16:02:57 +0530

    first.java

--- first.java
+++ first.java
@@ -4,7 +4,7 @@ class first {
         char a=0x0915;
         System.out.println(a);
 
-        /*
+        
          int x = 5;
          int y = -5;
          System.out.println(Integer.toBinaryString(x));
@@ -13,26 +13,26 @@ class first {
          System.out.println(Integer.toBinaryString(h));
          int b1=10;
          int b2= 0b1010;
-         * int b3= 012;
-         * int b4= 0XA;
-         * System.out.println(b1);
-         * System.out.println(b2);
-         * System.out.println(b3);
-         * System.out.println(b4);
-         * 
-         * float x=12.389f;
-         * System.out.println(x);
-         * char b='h';
-         * System.out.println(b);
-         * System.out.println("INT IS: " + Integer.MAX_VALUE);
-         * System.out.println("INT IS: " + Integer.MIN_VALUE);
-         * System.out.println("INT IS: " + Integer.BYTES);
-         * Scanner s = new Scanner(System.in);
-         * String name;
-         * System.out.println("enter a name");
-         * name=s.nextLine();
-         * System.out.println("the name is "+ name);
-         */
+          int b3= 012;
+          int b4= 0XA;
+          System.out.println(b1);
+          System.out.println(b2);
+          System.out.println(b3);
+          System.out.println(b4);
+          
+          float x=12.389f;
+          System.out.println(x);
+          char b='h';
+          System.out.println(b);
+          System.out.println("INT IS: " + Integer.MAX_VALUE);
+          System.out.println("INT IS: " + Integer.MIN_VALUE);
+          System.out.println("INT IS: " + Integer.BYTES);
+          Scanner s = new Scanner(System.in);
+          String name;
+          System.out.println("enter a name");
+          name=s.nextLine();
+          System.out.println("the name is "+ name);
+         
 
     }
 }
