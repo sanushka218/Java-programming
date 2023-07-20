4cc1c8717dd8387f8c7f0fc25ed573d43fe29cc4
Anushka Saxena <112062928+sanushka218@users.noreply.github.com>
2023-07-20 23:43:16 +0530

    Add files via upload

---
+++ test.java
@@ -0,0 +1,16 @@
+import pckgg.demo1;
+import pckgg.demo2;
+import pckgg.demo3;
+public class test {
+
+	public static void main(String[] args) {
+		demo1 d1=new demo1();
+		d1.display1();
+		demo2 d2= new demo2();
+		d2.display2();
+		demo3 d3= new demo3();
+		d3.display3();
+
+	}
+
+}
